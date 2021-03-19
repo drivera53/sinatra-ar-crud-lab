df6b6dcee8a5aa8981f79fb6307382064939bb25
Danny Rivera <samboard@hotmail.com>
2021-03-19 14:09:52 -0400

    commit tree 57eb04b3bc25cbf4bfa184910d3eecf001e8f235

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,58 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  ### INDEX ###
+  get "/" do
+    redirect to "/articles"
   end
+
+  ### CREATE ACTION ###
+  # new
+  get '/articles/new' do
+    @article = Article.new
+    erb :new
+  end
+
+  # create
+  post "/articles" do
+    # puts params
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+  
+
+  ### READ ACTION ###
+  # index
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  # show
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  ### UPDATE ACTION ###
+  # edits
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+  
+  # patch/update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  ### DELETE ###
+  #destroy
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end
+
 end
